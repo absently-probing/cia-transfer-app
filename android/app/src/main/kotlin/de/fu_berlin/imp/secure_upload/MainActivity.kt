dd0d9a1a15c69f4fa5e32a3235aa0d82ac0ce03d
Samuel Domiks <Kuerbisatom@protonmail.com>
2019-08-13 20:58:10 +0200

    Starter Package

---
+++ android/app/src/main/kotlin/de/fu_berlin/imp/secure_upload/MainActivity.kt
@@ -0,0 +1,13 @@
+package de.fu_berlin.imp.secure_upload
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
